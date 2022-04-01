3b00c3f8f6167e2dbead35313d9c792a2d3b19eb
Eros Chagas <eroschagas@hotmail.com>
2022-03-31 22:35:06 -0300

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    value: this.props.value,
+  };
+
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+  handleReduction = () => {
+    this.setState({ value: this.state.value - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          +
+        </button>
+        <button
+          onClick={this.handleReduction}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          -
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value: count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
